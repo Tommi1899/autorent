bbec6474e9534cf4ceae1087371e6315c4e209ae
elezar <omarabdullaev68@gmail.com>
2025-11-09 00:31:21 +0300

    init commit-2

--- backend/CarsRent/src/main/java/org/example/admin_module/order_module/order/Order.java
+++ backend/CarsRent/src/main/java/org/example/admin_module/order_module/order/Order.java
@@ -21,7 +21,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String balaljsdfljasdf;
     @ManyToOne
     @JoinColumn(name = "client_id", referencedColumnName = "id")
     private User client;
